1329642eb2694c2bd6e6c98bd3ba07efb276ba7c
Alshevsky <99246723+Alshevsky@users.noreply.github.com>
2022-09-26 19:15:09 +0300

    update

--- backend/garpix_notify/mixins/__init__.py
+++ backend/garpix_notify/mixins/__init__.py
@@ -1,2 +1,3 @@
 from .user_notify_mixin import UserNotifyMixin
-from .notify_mixin import NotifyMixin, NotifyMethodsMixin
+from .notify_mixin import NotifyMixin
+from .notify_method_mixin import NotifyMethodsMixin
